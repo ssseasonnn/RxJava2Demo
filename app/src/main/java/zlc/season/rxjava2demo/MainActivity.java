65f07336aaaf71389ccb3c680dce96897475eb3f
Season <ssseasonnn@gmail.com>
2016-12-15 11:50:33 +0800

    commit code

--- app/src/main/java/zlc/season/rxjava2demo/MainActivity.java
+++ app/src/main/java/zlc/season/rxjava2demo/MainActivity.java
@@ -2,12 +2,28 @@ package zlc.season.rxjava2demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
+import java.io.InterruptedIOException;
+
+import io.reactivex.functions.Consumer;
+import io.reactivex.plugins.RxJavaPlugins;
 import zlc.season.rxjava2demo.demo.ChapterSix;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "TAG";
+
+    static {
+        RxJavaPlugins.setErrorHandler(new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                if (throwable instanceof InterruptedIOException) {
+                    Log.d(TAG, "Io interrupted");
+                }
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
